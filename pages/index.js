c843a9155cbf46312d45bfc3377ef24d110d8995
kubo-hide-kun <hide44041@gmail.com>
2021-05-01 01:56:43 +0900

    Update lint-staged steps

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,11 @@ export default function Home() {
         <title>Create Next App</title>
 
         <link rel="icon" href="/favicon.ico" />
-
-
-
-
       </Head>
 
       <main className={styles.main}>
-        <h1 
-        className={styles.title}>
-
-    Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
