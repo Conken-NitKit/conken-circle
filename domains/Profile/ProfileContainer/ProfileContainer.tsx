62fee1ba259c3f70ed16f69fb77ff592eca67ef7
北代櫂 <0jr0572.7264b3x@ezweb.ne.jp>
2021-05-03 23:22:41 +0900

    feature: create profile-ui

---
+++ domains/Profile/ProfileContainer/ProfileContainer.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+import { BackHome } from "./BackHome";
+import { ProfileEdit } from "./ProfileEdit";
+
+const Icon = styled.div`
+  width: 10vw;
+  height: 10vw;
+  border-radius: 50%;
+  background: url("/profile.jpg");
+  object-fit: cover;
+  position: absolute;
+  margin-top: 7vw;
+`;
+
+const UserName = styled.div`
+  position: absolute;
+  top: 20vw;
+  font-size: 3vw;
+`;
+
+const BioGraphy = styled.div`
+  position: absolute;
+  top: 26vw;
+  font-size: 1.5vw;
+  width: 48%;
+`;
+
+const Follow = styled.p`
+  position: relative;
+  top: 18vw;
+  left: 15vw;
+  font-size: 1.3vw;
+`;
+
+export function ProfileContainer() {
+  return (
+    <>
+      <Icon />
+      <ProfileEdit />
+      <BackHome />
+      <UserName>ユーザー名</UserName>
+      <BioGraphy>
+        プロフィール
+        <br />
+        っっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっl
+      </BioGraphy>
+      <Follow>フォロー人</Follow>
+      <Follow>フォロワー人</Follow>
+    </>
+  );
+}
---
+++ domains/Profile/ProfileContainer/ProfileEdit/ProfileEdit.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const EditButton = styled.p`
+  position: absolute;
+  left: 30vw;
+  border: solid 1px black;
+  border-radius: 30px;
+  padding: 1vw 3vw;
+  font-size: 1.5rem;
+`;
+
+const ProfileText = styled.textarea`
+  height: 30vw;
+  width: 50vw;
+  font-size: 2vw;
+  border-radius: 1vw;
+  background-color: #808080;
+  color: white;
+`;
+
+const EditProfiler = styled.div<{ profiler: boolean }>`
+  position: absolute;
+  left: 20vw;
+  top: ${({ profiler }) => (profiler ? "8vh" : "-10000vh")};
+  transition: top 0.4s;
+`;
+
+const SaveButton = styled.div`
+  border: solid 1px black;
+  border-radius: 30px;
+  padding: 2% 8%;
+  width: 6%;
+  margin-top: 0.5vw;
+`;
+
+export function ProfileEdit() {
+  const [profiler, setProfiler] = useState<boolean>(false);
+
+  return (
+    <>
+      <EditButton onClick={() => setProfiler(true)}>Edit Profile</EditButton>
+      <EditProfiler profiler={profiler}>
+        <ProfileText placeholder={"素敵なプロフィールを書きましょう！"} />
+        <SaveButton onClick={() => setProfiler(false)}>SAVE</SaveButton>
+      </EditProfiler>
+    </>
+  );
+}
